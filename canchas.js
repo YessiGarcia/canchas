74baa592261a4144ff8d5a6f31dd80e0b0ca2a90
yessica <aria.801900@gmail.com>
2017-04-11 11:41:11 -0500

    son funcionales los dos botonesn

--- canchas.js
+++ canchas.js
@@ -1,20 +1,22 @@
 var botonCancha= document.getElementById("botonCancha");
+var botonBalon = document.getElementById("botonBalon");
+botonCancha.addEventListener("change", verCancha);
+botonBalon.addEventListener("change", verBalon);
+
+function verCancha (){
 var futbol=document.getElementById("futbol");
 var basquet=document.getElementById("basquet");
 var tenis=document.getElementById("tenis");
 
-botonCancha.addEventListener("change", verCancha);
-
-function verCancha (){
 	switch(this.value){
 		case "0":
 			futbol.setAttribute("hidden", true);
-			basquet.setAttribute("hidden" , true);
+			basquet.setAttribute("hidden", true);
 			tenis.setAttribute("hidden", true);
 			break;
 		case "1":
 			futbol.removeAttribute("hidden");
-			basquet.setAttribute("hidden" , true);
+			basquet.setAttribute("hidden", true);
 			tenis. setAttribute("hidden", true);
 			break;
 		case "2":
@@ -25,7 +27,37 @@ function verCancha (){
 		case "3":
 			tenis.removeAttribute("hidden");
 			futbol.setAttribute("hidden", true);
-			basquet.setAttribute("hidden" , true);
+			basquet.setAttribute("hidden", true);
 			break;
 	}
 }
+
+function verBalon() {
+var balonFut = document.getElementById("balonFut");
+var balonBasquet = document.getElementById("balonBasquet");
+var pelotaTenis = document.getElementById("pelotaTenis");
+
+	switch(this.value){
+		case "0":
+			balonFut.setAttribute("hidden", true);
+			balonBasquet.setAttribute("hidden", true);
+			pelotaTenis.setAttribute("hidden", true);
+			break;
+		case "1":
+			balonFut.removeAttribute("hidden");
+			balonBasquet.setAttribute("hidden", true);
+			pelotaTenis.setAttribute("hidden", true);
+			break;
+		case "2":
+			balonBasquet.removeAttribute("hidden");
+			balonFut.setAttribute("hidden", true);
+			pelotaTenis.setAttribute("hidden", true);
+			break;
+		case "3":
+			pelotaTenis.removeAttribute("hidden");
+			balonFut.setAttribute("hidden", true);
+			balonBasquet.setAttribute("hidden", true);
+			break;
+	}
+}
+
